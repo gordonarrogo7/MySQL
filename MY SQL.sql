5943d7fc29415aec506f1b8ece7caa7f4df44652
DoreenMuema <doreenmuema07@gmail.com>
2024-08-21 09:53:37 +0300

    Commits
    Commits

---
+++ MY SQL.sql
@@ -0,0 +1,25 @@
+CREATE TABLE donor_details (
+
+  id INT NOT NULL AUTO_INCREMENT,
+  
+  donor_name VARCHAR(50) NOT NULL,
+  
+  blood_type VARCHAR(5) NOT NULL,
+  
+  doner_age AGE NOT NULL,
+  
+  contact_number VARCHAR(20) NOT NULL,
+  
+  address VARCHAR(200) NOT NULL,
+  PRIMARY KEY (id)
+  
+  CREATE TABLE blood_bank (
+  
+  id INT NOT NULL AUTO_INCREMENT,
+  
+  blood_type VARCHAR(5) NOT NULL,
+  
+  collection_date DATE NOT NULL,
+  
+  place VARCHAR(Mumbai,Delhi,Kolkata)
+);
